fa57ecce1e4100466d61cbf3a4b48bf391fdf3e0
Mateussantos1207 <mateussntos99@gmail.com>
2026-08-15 19:36:18 -0300

    crud funcionando

--- logidutra/src/app/app.routes.ts
+++ logidutra/src/app/app.routes.ts
@@ -1,18 +1,10 @@
 import { Routes } from '@angular/router';
-
-import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { RomaneiosListComponent } from './components/romaneios/romaneios-list/romaneios-list.component';
 import { RomaneiosDetailsComponent } from './components/romaneios/romaneios-details/romaneios-details.component';
 
 export const routes: Routes = [
-  { path: 'login', component: RomaneiosListComponent },
-  {
-    path: '',
-    component: RomaneiosListComponent,
-    children: [
-      { path: 'romaneios', component: RomaneiosListComponent },
-      { path: 'romaneios/:id', component: RomaneiosDetailsComponent },
-    ]
-  },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+    { path: '', redirectTo: 'romaneios', pathMatch: 'full' },
+    { path: 'romaneios', component: RomaneiosListComponent },
+    { path: 'romaneios/new', component: RomaneiosDetailsComponent },
+    { path: 'romaneios/edit/:id', component: RomaneiosDetailsComponent },
 ];
--- logidutra/src/app/components/romaneios/romaneios-details/romaneios-details.component.ts
+++ logidutra/src/app/components/romaneios/romaneios-details/romaneios-details.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { FormsModule } from '@angular/forms';
+import { ItemEntrega } from '../../../models/item-entrega';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-romaneios-details',
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './romaneios-details.component.html',
   styleUrl: './romaneios-details.component.scss'
 })
 export class RomaneiosDetailsComponent {
+
+  titulo = 'cadastrar nova entrega'
+
+  entrega = new ItemEntrega(0, '', '', '');
+
+  route = inject(ActivatedRoute);
+  router = inject(Router);
+
+  constructor(){
+    let id = this.route.snapshot.params['id']
+    if (id > 0){
+      this.titulo = 'Editar entrega'
+      this.findById(id);
+    }
+  }
+
+  findById(id: number){
+    let entregaRetornada: ItemEntrega = new ItemEntrega(id, 'Joao silva', 'Roupeiro, sofa, comoda', 'Rua das flores, 123')
+    this.entrega = entregaRetornada;
+  }
+
+  salvar(){
+    if(this.entrega.id > 0){
+      Swal.fire({
+        title: 'Editado',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      });
+      this.router.navigate(['/romaneios'], {state: {entregaEditada: this.entrega}})
+    } else {
+      Swal.fire({
+        title: 'Salvo com sucesso',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      })
+    }
+  }
 
 }
