1273840da785475ac9f50f1509a70595c19770fd
Thales <thalesantos12@gmail.com>
2026-08-15 21:18:30 -0300

    feat: cria tela de recuperação de login com validação inicial
    - Cria interface com campo de usuário e botão de envio
    - Configura rota de navegação a partir da tela de login
    - Adiciona validação básica para impedir envio de campo vazio

--- logidutra/src/app/app.routes.ts
+++ logidutra/src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/layout/login/login.component';
+import { RecuperarSenhaComponent } from './components/layout/recuperar-senha/recuperar-senha.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch:'full'},
-    {path: 'login', component: LoginComponent}
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'recuperar-senha', component: RecuperarSenhaComponent }
+    // {
+    //     path: 'admin',
+    //     component: RomaneiosListComponent,
+    //     children: [
+    //         { path: 'romaneios', component: RomaneiosListComponent }
+    //     ]
+    // }
 ];
