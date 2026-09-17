24d0b27256f15b2403539860753bb15b4c211927
PauloBaier <baierpaulo312@gmail.com>
2026-09-17 20:49:14 -0300

    Navbar completa

--- logidutra/src/app/components/layout/nav-bar/nav-bar.component.ts
+++ logidutra/src/app/components/layout/nav-bar/nav-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown'
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MdbCollapseModule],
+  imports: [MdbCollapseModule, MdbDropdownModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
