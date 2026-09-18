9eea3426787fa5742896587b704e216bec814ee1
iagozlight <iago.chagas@escola.pr.gov.br>
2026-09-18 20:12:21 -0300

    Iniciado a tela de veiculos, apenas copiei e colei tudo o que tinha em usuarios, e fiz o modelo de veiculos também com tudo que está presente no back-end

--- logidutra/src/app/components/cadastros/veiculos/veiculos.component.ts
+++ logidutra/src/app/components/cadastros/veiculos/veiculos.component.ts
@@ -1,5 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { veiculo } from '../../../models/veiculos';
 
 @Component({
   selector: 'app-veiculos',
@@ -8,5 +8,39 @@ import { Router } from '@angular/router';
   styleUrl: './veiculos.component.scss'
 })
 export class VeiculosComponent {
-  router = inject(Router);
+  veiculos: veiculo[] = [];
+  paginaAtual = 1;
+  MaxPag = 6;
+
+  constructor() {
+    const veiculosSalvos = sessionStorage.getItem('veiculos');
+    this.veiculos = veiculosSalvos ? JSON.parse(veiculosSalvos) : [];
+  }
+
+  get veiculosPaginados(): veiculo[] {
+    const inicio = (this.paginaAtual - 1) * this.MaxPag;
+    return this.veiculos.slice(inicio, inicio + this.MaxPag);
+  }
+
+  get totalPaginas(): number {
+    return Math.ceil(this.veiculos.length / this.MaxPag) || 1;
+  }
+
+  get paginas(): number[] {
+    return Array.from({ length: this.totalPaginas }, (_, indice) => indice + 1);
+  }
+
+  irParaPagina(pagina: number): void {
+    if (pagina >= 1 && pagina <= this.totalPaginas) {
+      this.paginaAtual = pagina;
+    }
+  }
+
+  paginaAnterior(): void {
+    this.irParaPagina(this.paginaAtual - 1);
+  }
+
+  paginaProxima(): void {
+    this.irParaPagina(this.paginaAtual + 1);
+  }
 }
